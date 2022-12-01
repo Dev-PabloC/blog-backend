4ad2ab4d4b1bc52ecde088eb5634d20083d41e51
Pablo h.c <pablo.dev.carvalho@gmail.com>
2022-12-01 01:05:44 -0300

    prisma middleware password hash

--- src/database/prismaconnection.ts
+++ src/database/prismaconnection.ts
@@ -1,3 +1,13 @@
 import { PrismaClient } from "@prisma/client";
+import bcrypt from "bcrypt"
 
 export const prisma = new PrismaClient();
+
+prisma.$use(async (params, next) => {
+    if (params.model === "User" && params.action === "create" || params.action === "update" && params.args.data.password) {
+        const hashPassword = await bcrypt.hash(params.args.data.password, 12)
+        params.args.data.password = hashPassword
+    }
+
+    return next(params)
+})
